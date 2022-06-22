cb7cb052cea262e00197242fe8be533f2ae2a055
Ratttz <107441452+Ratttz@users.noreply.github.com>
2022-06-21 17:33:09 -0700

    FBTimer 0.91
    Features:
    
    Added the ability to synchronize the timer to loading a savestate. The default behavior is to load savestates saved using the script only; this is by design to prevent users from messing up their savestates, and to keep savestates used by the timer siloed away from the others.
    
    Bugfixes:
    
    Users are now no longer able to add or remove time from the timer while the timer is running.

--- FBTimer.lua
+++ FBTimer.lua
@@ -1,33 +1,73 @@
+--Initializing variables
+
 endgamenow = false
 length = 18000
+timerisrunning = false
+
+--I recommend that you change the name of this script and "FBTimer_" within targetstate to the name of your event.
+--By doing it this way, your players/users never have to overwrite or manage their savestates,
+--provided that they are always given the current savestate and script.
+targetstate = ("FBTimer_"..fba.gamename()..".fs")
+
+--The following is a list of functions intended to be operated by the user via hotkeys
 
 function addfive()
-	length = length + 18000
+	if timerisrunning == false then
+		length = length + 18000
+	end
 end
 	
 function subfive()
-	length = length - 18000
+	if timerisrunning == false then
+		length = length - 18000
+	end
 end
 
 function endgame()
-	endgamenow = true
+	if timerisrunning == true then
+		endgamenow = true
+		end
 end
+
+function newsavestate()
+	newstate = savestate.create(targetstate)
+	savestate.save(newstate)
+	print("saved!")
+
+end
+
+function loadstate()
+	if timerisrunning == false then
+		savestate.load(targetstate)
+		print("loaded")
+		local newpad = joypad.read()
+		newpad["P1 Start"] = true
+		joypad.set(newpad)
+	end
+end
+
+--registering those functions as hotkeys
 
 input.registerhotkey(1, addfive)
 input.registerhotkey(2, subfive)
 input.registerhotkey(3, endgame)
+input.registerhotkey(4, newsavestate)
+input.registerhotkey(5, loadstate)
 
+--Begin waiting on the player to do stuff
 
 while joypad.read()["P1 Start"] == false do
 	if length < 18000 then
 		length = 18000
 	end
-    gui.text(50, 0, "Waiting for start...\nTimer set for "..5*(length/18000).." minutes.\nPress Hotkey 1 to add five minutes\nPress Hotkey 2 to deduct five minutes\nPress Hotkey 3 to end your run early.")
+    gui.text(50, 0, "Waiting for start...\nTimer set for "..5*(length/18000).." minutes.\nPress Hotkey 1 to add five minutes\nPress Hotkey 2 to deduct five minutes\nPress Hotkey 3 to end your run early.\nPress Hotkey 4 to save a state with default settings.\nPress Hotkey 5 to start your run from the saved state.")
 	emu.frameadvance()
 end
 
-start = emu.framecount()
+--They did stuff, start the timer.
 
+start = emu.framecount()
+timerisrunning = true
 
 while (emu.framecount() - start) <= length and (endgamenow == false) do
 	local timekeeper = ((emu.framecount() - start) .. " / " .. length)
@@ -35,6 +75,8 @@ while (emu.framecount() - start) <= length and (endgamenow == false) do
 	gui.text(50, 0, timekeeper)
 	emu.frameadvance()
 end
+
+--End their run.
 
 do
 	gui.text(50, 0, "Congratulations on finishing your run!")
